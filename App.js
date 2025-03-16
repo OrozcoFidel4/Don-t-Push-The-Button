8679240c05b918afcfdcf3109124da8446efd716
OrozcoFidel4 <fidel21304022@gmail.com>
2025-03-16 14:56:55 -0700

    Version 1.0.0

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 
 export default function App() {
+
+  const ButtonAlert = () =>
+    Alert.alert('I told you', "Don't push again", [
+      {text: 'OK'},
+    ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.descriptionText}>Don't push the button</Text>
+      
+      <TouchableOpacity style={styles.button} onPress={ButtonAlert}>
+        <Text style={styles.buttonText}>Don't Push</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +28,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  descriptionText:{
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+
+  button:{
+    margin: 20,
+    backgroundColor: 'red',
+    width: '25%',
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: 'black',
+    borderWidth: 2
+  },
+
+  buttonText: {
+    color: 'white'
+
+  }
+
 });
